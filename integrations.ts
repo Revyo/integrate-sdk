f50f3623dff1a353a7b82a906e80018e948bb4a0
Jeremy Bosma <jectaunscripted@gmail.com>
2025-11-14 16:59:15 +0100

    init

---
+++ integrations.ts
@@ -0,0 +1,9 @@
+/**
+ * AI Provider Integrations Entry Point
+ * 
+ * This is the main entry point for AI provider integrations.
+ * Import from 'integrate-sdk/integrations' to get access to all provider integrations.
+ */
+
+export * from './src/integrations/index.js';
+
