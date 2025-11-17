39ed40fe9733d90ef8440145fdbdebf439940b97
Jeremy Bosma <jectaunscripted@gmail.com>
2025-11-17 02:29:05 +0100

    simpler exports

---
+++ src/adapters/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Adapters for various frameworks
+ * 
+ * Re-exports all adapter modules for convenient access
+ */
+
+// Re-export all adapters
+export * from "./nextjs.js";
+export * from "./node.js";
+export * from "./solid-start.js";
+export * from "./svelte-kit.js";
+export * from "./tanstack-start.js";
+export * from "./base-handler.js";
+export * from "./auto-routes.js";
+
